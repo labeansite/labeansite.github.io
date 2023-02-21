2167f73d8f81d8850a78744b9b3fc35612c091b4
Thiago Maximo Pavão <thipavao44@gmail.com>
2023-02-21 11:10:25 -0300

    navbar menu

--- js/navbar-mobile-menu.js
+++ js/navbar-mobile-menu.js
@@ -1,3 +1,8 @@
-$('.navbar-toggle').click(function() {
-    $('.navbar-container').toggleClass('show');
+// Get the elements
+const navbarContainer = document.querySelector('.navbar-container');
+const navbarToggle = document.querySelector('.navbar-toggle');
+
+// Add an event listener to the toggle button
+navbarToggle.addEventListener('click', function() {
+  navbarContainer.classList.toggle('show');
 });
