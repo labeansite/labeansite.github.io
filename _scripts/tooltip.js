d2a8b1576b279142c39a6e34f105d39e21a428a2
Thiago Maximo Pavão <thipavao44@gmail.com>
2023-03-11 21:55:49 -0300

    heading links improved

--- _scripts/tooltip.js
+++ _scripts/tooltip.js
@@ -3,34 +3,49 @@
   attribute.
 */
 
+function clipboardTippy(e) { // define here because it`s used in anchors.js
+  tippy(e, {
+    content: (element) => element.dataset.clipboardText.trim(),
+    trigger: 'mouseenter click',
+    delay: [100, 0],
+    offset: [0, 20],
+    allowHTML: true,
+    interactive: true,
+    appendTo: () => document.body,
+    onShow(instance) {
+      // Reset the tooltip content on each show
+      instance.setContent(instance.reference.dataset.clipboardText.trim() + ' (Click to copy)');
+    },
+    onTrigger(instance, event) {
+      if (event.type === 'click') {
+        // Copy the text content to the clipboard
+        navigator.clipboard.writeText(instance.reference.dataset.clipboardText.trim())
+          .then(() => {
+            // Set the tooltip content to the copy feedback message
+            instance.setContent(instance.reference.dataset.clipboardText.trim() + ' (Copied!)');
+            setTimeout(() => {
+              instance.hide();
+            }, 1000);
+          })
+          .catch(() => {
+            instance.setContent(instance.reference.dataset.clipboardText.trim() + ' (Copy failed D:)');
+          });
+      }
+    }
+  });
+}
+
 {
   const onLoad = () => {
     
     // make sure Tippy library available and the user
     if (typeof tippy === "undefined") return;
 
-    tippy('.clipboard-button', {
-      content: 'Click to copy', 
-      trigger: 'mouseenter click',
-      interactive: true,
-      onShow(instance) {
-        // Reset the tooltip content on each show
-        instance.setContent(instance.reference.dataset.clipboardText + ' (Click to copy)');
-      },
-      onTrigger(instance, event) {
-        if (event.type === 'click') {
-          // Copy the text content to the clipboard
-          navigator.clipboard.writeText(instance.reference.dataset.clipboardText)
-            .then(() => {
-              // Set the tooltip content to the copy feedback message
-              instance.setContent(instance.reference.dataset.clipboardText + ' (Copied!)');
-            })
-            .catch(() => {
-              instance.setContent(instance.reference.dataset.clipboardText + ' (Copy failed D:)');
-            });
-        }
-      }
-    });
+    const elementsToClipboard = [...document.querySelectorAll("[data-clipboard-text]")].filter(
+      (element) => element.dataset.clipboardText.trim() && !element._tippy
+    );
+
+    clipboardTippy(elementsToClipboard)
 
     // Check if the user is on a mobile device
     const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
@@ -45,7 +60,7 @@
     // add tooltip to elements
     tippy(elements, {
       content: (element) => element.dataset.tooltip.trim(),
-      delay: [200, 0],
+      delay: [100, 0],
       offset: [0, 20],
       allowHTML: true,
       interactive: true,
